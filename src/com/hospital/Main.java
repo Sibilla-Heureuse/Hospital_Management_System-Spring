48e7b9459d57134da0008a3f1458fccfef6983b3
Sibilla-Heureuse <sibillaheureuse@gmail.com>
2026-03-12 20:12:46 +0200

    commit with the console

--- src/com/hospital/Main.java
+++ src/com/hospital/Main.java
@@ -1,9 +1,39 @@
 package com.hospital;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
+import com.hospital.config.AppConfig;
+import com.hospital.model.*;
+import com.hospital.service.HospitalService;
+import java.sql.Timestamp;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HospitalService service =
+                context.getBean(HospitalService.class);
+
+        Doctor doctor =
+                new Doctor("John","Smith","Cardiology","john@gmail.com");
+
+        Patient patient =
+                new Patient("Alice","Brown","Female","alice@gmail.com");
+
+        Appointment appointment =
+                new Appointment(1,1,new Timestamp(System.currentTimeMillis()),"Scheduled");
+
+        MedicalRecord record =
+                new MedicalRecord(1,1,"Flu","Medication");
+
+        service.saveDoctor(doctor);
+        service.savePatient(patient);
+        service.saveAppointment(appointment);
+        service.saveRecord(record);
+
+        System.out.println("Data inserted successfully!");
     }
 }
